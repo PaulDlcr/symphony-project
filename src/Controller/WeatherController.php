994e05f02df9aa60a67b0c1f634ff4f3e29e896e
PaulDlcr <delacrepaul@gmail.com>
2023-01-04 15:48:15 +0100

    Added LuckyNumber and weather

---
+++ src/Controller/WeatherController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+
+class WeatherController extends AbstractController
+{
+    //use OpenWeatherMap API
+    #[Route('/weather')]
+function index(): Response
+    {
+    $apiKey = "0681ce78641fabbc31b7640408d6cac8";
+    $location = 'Paris';
+    $url = 'https://api.openweathermap.org/data/2.5/weather?' .
+    'q=' . urlencode($location) .
+        '&appid=' . $apiKey .
+        '&lang=fr';
+    $response = file_get_contents($url);
+    $weatherData = json_decode($response, true);
+    $temperature = $weatherData['main']['temp'] - 273.15;
+    $description = $weatherData['weather'][0]['description'];
+    $wind = $weatherData['wind']['speed'];
+    return $this->render('lucky/weather.html.twig', [
+        'temperature' => $temperature,
+        'description' => $description,
+        'wind' => $wind,
+    ]);
+}
+
+}
